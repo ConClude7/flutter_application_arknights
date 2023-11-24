aec4d66bf6881900f25b7c4bb858081f1a40f106
YKX <15381882756@163.com>
2023-11-24 10:25:22 +0800

    version_1.0.0 仓库初始化

--- lib/common/global.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_application_arknights/models/index.dart';
-
-class global {
-  static late User user;
-}
--- lib/widgets/common/Refreshing.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget RefreshWidget = const CircularProgressIndicator(
-  color: Color.fromARGB(255, 252, 163, 17),
-);
